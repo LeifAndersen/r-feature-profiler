fc00c021c1dfb4c7d29b37035dd3db3e370c8eca
Leif Andersen <leif@leifandersen.net>
2015-04-18 01:21:18 -0400

    First pass on analysis.

--- feature-profile.R
+++ feature-profile.R
@@ -1,29 +1,43 @@
-library(base64enc);
+## Feature Specific Profiling for R language
+##
+## This file does analyses the results collected from Rprof.
+##
+## To collect samples with Rprof, do something like this:
+## Rprof("profile1.out")
+## <code-here>
+## Rprof(NULL)
+##
+## Analyse results with feature.profile()
 
+library(base64enc);
 
 ## Collect the marks from a Rprof file,
 ## for use by analyzer
 collect.marks <- function (filename = "Rprof.out",
+                           features = c(),
                            chunksize = 5000) {
 
     ## Check the log type, ensure it contains continuation marks, get sample interval
     con <- file(filename, "rt");
     on.exit(close(con));
     firstline <- readLines(con, n = 1L);
-    if(!length(firstline))
+    if(!length(firstline)) {
         stop(gettextf("no lines found in %s", sQuote(filename)), domain = NA);
+    }
     sample.interval <- as.numeric(strsplit(firstline, "=")[[1L]][2L])/1e6
     marks.profiling <- grepl("marks profiling", firstline)
-    if(!marks.profiling)
+    if(!marks.profiling) {
         stop(gettextf("Marks not recorded in %s", sQuote(filename)), domain = NA);
+    }
 
     traces <- list()
 
     ## Grab file in chunks
     repeat {
        chunk <- readLines(con, n = chunksize);
-       if (length(chunk) == 0L)
-           break
+       if (length(chunk) == 0L) {
+           break;
+       }
 
        ## Grab list of continuation marks
        chunk <- strsplit(chunk, " ")
@@ -31,13 +45,15 @@ collect.marks <- function (filename = "Rprof.out",
            lapply(trace[grepl("^marks\\{", trace)], function(marks) {
                marks <- substr(marks, 7, nchar(marks) - 2);
                marks <- strsplit(marks, ",");
-               res <- new.env()
+               res <- c();
                for(mark in marks) {
                    mark <- substr(mark, 1, nchar(mark) - 1);
                    mark <- strsplit(mark, ":")[[1]];
                    key <- rawToChar(base64decode(mark[1]));
                    value <- rawToChar(base64decode(mark[2]));
-                   res[[key]] <- value;
+                   if(is.element(key, features)) {
+                       res[[key]]<-value;
+                   }
                }
                res;
            })
@@ -47,14 +63,54 @@ collect.marks <- function (filename = "Rprof.out",
        traces <- c(traces, chunk);
    }
 
-
     ## Return result of parse
-    traces
+    list(traces=traces,interval=sample.interval);
 }
 
-feature.profile <- function(body, tags) {
-
+filter.mark <- function(traces, mark) {
+    lapply(traces, function(trace) {
+        trace <- lapply(trace, function(frame) {
+            frame[[mark]];
+        })
+        Filter(Negate(is.null), trace);
+    })
 }
 
+feature.profile <- function(filename = "Rprof.out",
+                            features = c(),
+                            chunksize = 5000) {
 
-collect.marks("profile1.out")
+    ## Collect Samples
+    samples <- collect.marks(filename, features, chunksize);
+    traces <- samples$traces;
+    interval <- samples$interval;
+
+    ## Calculate times
+    exectime <- interval * length(traces);
+    featureAnalysis <- lapply(setNames(features,features), function(feature) {
+        featureMarks <- filter.mark(traces,feature);
+
+        ## Get the bottom mark of each of the stacks
+        bottomMark <- sapply(featureMarks, function(trace) {
+            if(length(trace) > 0) {
+                trace[[length(trace)]];
+            }
+        })
+
+        featureTime <- length(Filter(Negate(is.null), bottomMark))*interval;
+        table(unlist(bottomMark))*interval;
+    })
+
+    ## Return Results
+    list(samples=length(traces),
+         time=exectime,
+         features=featureAnalysis);
+}
+
+## x <- collect.marks("profile1.out", features=c("foo"))
+## y <- filter.mark(x$traces,"foo");
+## y
+
+x <- feature.profile(filename = "profile1.out",
+                     features = c("foo"));
+x
